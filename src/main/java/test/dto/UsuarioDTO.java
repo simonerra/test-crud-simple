cbe110d8a76d5295132899bb2a78f39edb954fc9
Simón Errázuriz <simonerrazurizrubilar@gmail.com>
2023-09-11 01:00:58 -0300

    fix: added constructors with all arguments for UserEntity and UserDTO

--- src/main/java/test/dto/UsuarioDTO.java
+++ src/main/java/test/dto/UsuarioDTO.java
@@ -1,9 +1,11 @@
 package test.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class UsuarioDTO {
 	private Long id;
--- src/main/java/test/entity/UsuarioEntity.java
+++ src/main/java/test/entity/UsuarioEntity.java
@@ -6,10 +6,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "usuarios")
